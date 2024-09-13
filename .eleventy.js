8f9965d0981d6acb3e28eea2406124565214d8ef
nfisherman <ca.lehman05@gmail.com>
2024-09-13 18:20:53 -0500

    added defaults for hero

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./static/");
     
+    // Defaults
+    eleventyConfig.addGlobalData("hero.img", "/static/img/hero/index.png");
+    eleventyConfig.addGlobalData("hero.color", "red-tint-1");
+
     return {
         dir: {
             input: "src",
