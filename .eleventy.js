46f4961f037e37efd14770039f90eeb0196c20ee
nfisherman <ca.lehman05@gmail.com>
2024-09-13 18:36:03 -0500

    added default favicon and org

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./static/");
     
     // Defaults
+    eleventyConfig.addGlobalData("favicon", "/static/img/favicon.png");
+    eleventyConfig.addGlobalData("org", "Milwaukee YDSA");
     eleventyConfig.addGlobalData("hero.img", "/static/img/hero/index.png");
     eleventyConfig.addGlobalData("hero.color", "red-tint-1");
 
