a142559808665498bee6e6f0145640b02f57db59
nfisherman <ca.lehman05@gmail.com>
2024-09-14 15:49:54 -0500

    added Decap CMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./admin/");
     eleventyConfig.addPassthroughCopy("./static/");
     eleventyConfig.addPassthroughCopy("./LICENSE");
     
