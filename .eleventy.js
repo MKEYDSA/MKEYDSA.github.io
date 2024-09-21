cd2cabeaf253db53e6760255c0b57dd856e49d08
nfisherman <ca.lehman05@gmail.com>
2024-09-21 18:51:19 -0500

    added return to home page

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addGlobalData( "favicon-32", "/static/img/icon/favicon-32.ico");
     eleventyConfig.addGlobalData("favicon-192", "/static/img/icon/favicon-192.ico");
     eleventyConfig.addGlobalData(        "org", "Milwaukee YDSA");
-    eleventyConfig.addGlobalData(  "shortcode", "MKE");
+    eleventyConfig.addGlobalData(  "shortcode", "mke");
     eleventyConfig.addGlobalData(       "logo", "/static/img/icon/logo.svg")
     eleventyConfig.addGlobalData(   "hero.img", "/static/img/hero/index.png");
     eleventyConfig.addGlobalData( "hero.color", "red-tint-1");
